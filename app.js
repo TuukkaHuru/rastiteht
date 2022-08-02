e8eafcd716c30c44757bb72fddcebc23d218443e
TuukkaHuru <t1hutu02@students.oamk.fi>
2022-08-02 19:17:07 +0300

    koodia tehty

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+const bookRouter = require('./routes/book');
+const carRouter = require('./routes/car');
 
 var app = express();
 
@@ -13,8 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/car', carRouter);
 
 module.exports = app;
